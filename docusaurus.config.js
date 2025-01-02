4cd34774b8d0cd3a840f2afb4b62393b9725d8e4
haobiao.ning <774981807@qq.com>
2025-01-02 12:15:15 +0800

    fix update for githubPage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/e3Guide/',
-
+  deploymentBranch: 'master',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Goldlions', // Usually your GitHub org/user name.
